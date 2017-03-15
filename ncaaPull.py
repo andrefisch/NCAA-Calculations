120ccfbe4db30c77e202b6f3eca43ffdb9584842
Andrew Fischl <andrefisch@github.com>
2017-03-15 00:23:20 -0400

    exchanged a few constants for variables

--- ncaaPull.py
+++ ncaaPull.py
@@ -7,14 +7,17 @@ html = pandas.read_html("http://escrimeresults.com/NCAA/ncaa2016.html")
 # Read schools into dictionary
 # works correctly #######################################################
 schools = {}
+schoolColumn = 0
+victoryColumn = 27
+otherFencers = 23
 for i in range(1, 25):
     for j in range (1, 7):
-        temp = html[j * 3 - 1].iloc[i, 0]
+        temp = html[j * 3 - 1].iloc[i, schoolColumn]
         if (temp in schools.keys()):
-            schools[temp] = [schools[temp][0], schools[temp][1] + 23]
+            schools[temp] = [schools[temp][0], schools[temp][1] + otherFencers]
         else:
-            schools[temp] = [0, 23]
-        num = html[j * 3 - 1].iloc[i, 27]
+            schools[temp] = [0, otherFencers]
+        num = html[j * 3 - 1].iloc[i, victoryColumn]
         schools[temp] = [schools[temp][0] + int(num), schools[temp][1]]
 #########################################################################
 schools = sorted(schools.items(), key=lambda i: i[1][0], reverse=True)
