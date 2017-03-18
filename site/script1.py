98ddc8c5cabed24b6b640250727677015d650753
Andrew Fischl <anfischl@gmail.com>
2017-03-18 14:23:24 -0400

    put header menu bar above the logo and removed the word logo from the tables. a few bug fixes as well

--- site/script1.py
+++ site/script1.py
@@ -29,11 +29,12 @@ def home():
         # 3: % of bouts needed to clinch/catch leader
         boutsRemain[temp] = [int(html[18].iloc[i, totalColumn]), int(html[18].iloc[i, remainColumn]), 0, 0.0, ""]
     # ADD A LITTLE FAKE DATA FOR TESTING
-    boutsRemain["Columbia/Barnard"][1] = 10
-    boutsRemain["Ohio State University"][1] = 10
-    boutsRemain["Princeton University"][1] = 20
-    boutsRemain["St. John's University"][1] = 14
-    boutsRemain["Notre Dame"][1] = 20
+    # boutsRemain["Columbia/Barnard"][1] = 10
+    # boutsRemain["Ohio State University"][1] = 10
+    # boutsRemain["Princeton University"][1] = 20
+    # boutsRemain["St. John's University"][1] = 14
+    # boutsRemain["Notre Dame"][1] = 20
+    boutsRemain["Lawrence University"][1] = 200
     # sort the bouts and store information in an array
     schools = len(boutsRemain)
     boutsRemain = sorted(boutsRemain.items(), key=lambda i: i[1][0], reverse=True)
@@ -53,7 +54,10 @@ def home():
             need = 1 + int(boutsRemain[i][1][0]) + int(boutsRemain[i][1][1]) - int(boutsRemain[0][1][0])
             remain = int(boutsRemain[0][1][1])
             boutsRemain[0][1][2] = need
-            boutsRemain[0][1][3] = str(round(need / remain * 100.0, 1)) + "%"
+            if (remain > 0):
+                boutsRemain[0][1][3] = str(round(need / remain * 100.0, 1)) + "%"
+            else:
+                boutsRemain[0][1][3] = "DONE"
             break
     if (isOver):
         boutsRemain[0][1][2] = "WINNER, WINNER!"
@@ -67,7 +71,7 @@ def home():
         firstRemain = int(boutsRemain[0][1][1])
         # firstPercent = firstNeed / firstRemain * 100.0
         if (diff > int(boutsRemain[i][1][1])):
-            boutsRemain[i][1][2] = "OUT"
+            boutsRemain[i][1][2] = diff
             boutsRemain[i][1][3] = "OUT"
         else:
             if (iRemain > 0):
@@ -75,7 +79,7 @@ def home():
                 boutsRemain[i][1][2] = diff
                 boutsRemain[i][1][3] = str(iPercent) + "%"
             else:
-                boutsRemain[i][1][2] = "OUT"
+                boutsRemain[i][1][2] = diff
                 boutsRemain[i][1][3] = "OUT"
     # name in quotes must be inside templates folder
     return render_template("home.html", boutsRemain=boutsRemain)
