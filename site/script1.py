e147967a681d8d6297120141d3a240771c6063d1
Andrew Fischl <anfischl@gmail.com>
2017-03-18 12:12:03 -0400

    logos now appear next to schools, added a percent sign next to percentages but not next to OUT

--- site/script1.py
+++ site/script1.py
@@ -4,17 +4,15 @@ import operator
 
 app = Flask(__name__)
 
+# Import the dictionary file
+schoolConversion = {}
+with open('static/text/schoolDict.txt') as f:
+    for line in f:
+       (key, val) = line.strip('\n').split(";")
+       schoolConversion[key] = val
+
 @app.route('/')
 def home():
-    # name in quotes must be inside templates folder
-    return render_template("home.html")
-
-@app.route('/about/')
-def about():
-    return render_template("about.html")
-
-@app.route('/display/')
-def display():
     # Get the information and make the dictionary
     html = pandas.read_html("http://escrimeresults.com/NCAA/ncaa2016.html")
     # Create bouts remain dictionary
@@ -29,7 +27,7 @@ def display():
         # 1: bouts remaining
         # 2: bouts to clinch/bouts behind
         # 3: % of bouts needed to clinch/catch leader
-        boutsRemain[temp] = [int(html[18].iloc[i, totalColumn]), int(html[18].iloc[i, remainColumn]), 0, 0.0]
+        boutsRemain[temp] = [int(html[18].iloc[i, totalColumn]), int(html[18].iloc[i, remainColumn]), 0, 0.0, ""]
     # ADD A LITTLE FAKE DATA FOR TESTING
     boutsRemain["Columbia/Barnard"][1] = 10
     boutsRemain["Ohio State University"][1] = 10
@@ -39,8 +37,12 @@ def display():
     # sort the bouts and store information in an array
     schools = len(boutsRemain)
     boutsRemain = sorted(boutsRemain.items(), key=lambda i: i[1][0], reverse=True)
-    # determine wins needed and win percentage information
-    # find difference in wins between first and second team
+    # make sure references to image files are part of array
+    # boutsRemain[0][1][4] = schoolConversion[boutsRemain[0][0]] + ".png"
+    for i in range (0, schools):
+        # make sure references to image files are part of array
+        boutsRemain[i][1][4] = schoolConversion[boutsRemain[i][0]] + ".png"
+    # find win differences in wins for teams and fill out array information 
     isOver = True
     for i in range (1, schools):
         # if the difference is greater than all other schools' remaining bouts to fence then it is over
@@ -51,7 +53,7 @@ def display():
             need = 1 + int(boutsRemain[i][1][0]) + int(boutsRemain[i][1][1]) - int(boutsRemain[0][1][0])
             remain = int(boutsRemain[0][1][1])
             boutsRemain[0][1][2] = need
-            boutsRemain[0][1][3] = round(need / remain * 100.0, 1)
+            boutsRemain[0][1][3] = str(round(need / remain * 100.0, 1)) + "%"
             break
     if (isOver):
         boutsRemain[0][1][2] = "WINNER, WINNER!"
@@ -71,12 +73,19 @@ def display():
             if (iRemain > 0):
                 iPercent = round(diff / iRemain * 100.0, 1)
                 boutsRemain[i][1][2] = diff
-                boutsRemain[i][1][3] = iPercent
+                boutsRemain[i][1][3] = str(iPercent) + "%"
             else:
                 boutsRemain[i][1][2] = "OUT"
                 boutsRemain[i][1][3] = "OUT"
-    # pass it to function
-    return render_template("display.html", boutsRemain=boutsRemain)
+    # name in quotes must be inside templates folder
+    return render_template("home.html", boutsRemain=boutsRemain)
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+# @app.route('/display/')
+# def display():
 
 if __name__ == "__main__":
     app.run(debug = True)
