cbebff98be9aebaf4a26f8778cf3f976934f778a
Andrew Fischl <anfischl@gmail.com>
2017-03-23 12:07:35 -0400

    added percents but not working

--- server.py
+++ server.py
@@ -13,11 +13,10 @@ with open("./static/text/schoolDict.txt") as f:
         schoolConversion[key] = val
 fencersPerSchool = {}
 # Import the dictionary file for number of fencers per school
-'''
-with open("./static/text/numFencers.txt") as f:
+with open("./static/text/totalFencers.txt") as f:
     for line in f:
         (key, val) = line.strip('\n').split(";")
-'''
+        fencersPerSchool[key] = val
 # Import list of Andrew quips
 andrewList = []
 with open("./static/text/andrewList.txt") as f:
@@ -116,6 +115,12 @@ def home():
                 else:
                     boutsRemain[i][1][2] = diff
                     boutsRemain[i][1][3] = "OUT"
+        # get percentage of wins so far
+        for i in range (0, schools):
+            # current wins / (total - remaining) * 100.0
+            totalBouts = (int(fencersPerSchool[boutsRemain[i][0]]) * 23)
+            percent = round(boutsRemain[i][1][0] / (totalBouts - boutsRemain[i][1][1]) * 100.0, 1)
+            boutsRemain[i][1][0] = str(boutsRemain[i][1][0]) + " (" + str(percent) + ")"
         # for i in range(0, schools):
             # if (int(boutsRemain[i][1][1]) == 0):
                 # boutsRemain[i][1][1] == "DONE"
@@ -131,97 +136,6 @@ def about():
     elijah = random.choice(elijahList)
     return render_template("about.html", andrew=andrew, elijah=elijah)
 
-@app.route('/display/')
-def display():
-    # Get the information and make the dictionary
-    # 2016 final version
-    # html = pandas.read_html("http://escrimeresults.com/NCAA/ncaa2016.html")
-    # ARCHIVED VERSION WORKS
-    html = pandas.read_html("http://web.archive.org/web/20160325002832/http://www.escrimeresults.com/NCAA/NCAA2016.html")
-    # EMPTY DOESNT WORK
-    # html = pandas.read_html("http://web.archive.org/web/20160324121938/http://www.escrimeresults.com/NCAA/NCAA2016.html")
-    # Create bouts remain dictionary
-    boutsRemain = {}
-    # go through list of schools at bottom of page
-    if (len(html[-2]) > 2):
-        for i in range(1, len(html[-2])):
-            schoolColumn = 2
-            totalColumn = 3
-            remainColumn = 4
-            temp = html[-2].iloc[i, schoolColumn].strip('\n')
-            # 0: total wins
-            # 1: bouts remaining
-            # 2: bouts to clinch/bouts behind
-            # 3: % of bouts needed to clinch/catch leader
-            # 4: name of png for school logo
-            # 5: current ranking
-            boutsRemain[temp] = [int(html[-2].iloc[i, totalColumn]), int(html[-2].iloc[i, remainColumn]), 0, 0.0, "", 0]
-        # ADD A LITTLE FAKE DATA FOR TESTING
-        # {{{
-        # boutsRemain["Columbia/Barnard"][0] = 163
-        # boutsRemain["Ohio State University"][1] = 10
-        # boutsRemain["Princeton University"][1] = 20
-        # boutsRemain["St. John's University"][1] = 14
-        # boutsRemain["Notre Dame"][1] = 20
-        # boutsRemain["Lawrence University"][1] = 200
-        # }}}
-        # sort the bouts and store information in an array
-        schools = len(boutsRemain)
-        boutsRemain = sorted(boutsRemain.items(), key=lambda i: i[1][0], reverse=True)
-        # make sure references to image files are part of array
-        # boutsRemain[0][1][4] = schoolConversion[boutsRemain[0][0]] + ".png"
-        for i in range (0, schools):
-            # make sure references to image files are part of array
-            boutsRemain[i][1][4] = schoolConversion[boutsRemain[i][0]] + ".png"
-            # what is the current school's place
-            boutsRemain[i][1][5] = i + 1
-        # find win differences in wins for teams and fill out array information 
-        isOver = True
-        for i in range (1, schools):
-            # if the difference is greater than all other schools' remaining bouts to fence then it is over
-            diff = int(boutsRemain[0][1][0]) - int(boutsRemain[i][1][0])
-            if (diff < int(boutsRemain[i][1][1])):
-                isOver = False
-                # bouts needed to clinch for top school becomes ith place's win total + ith place's remaining bouts - 1st place total bouts
-                need = 1 + int(boutsRemain[i][1][0]) + int(boutsRemain[i][1][1]) - int(boutsRemain[0][1][0])
-                remain = int(boutsRemain[0][1][1])
-                if (remain > 0):
-                    boutsRemain[0][1][2] = need
-                    boutsRemain[0][1][3] = str(round(need / remain * 100.0, 1)) + "%"
-                else:
-                    boutsRemain[0][1][2] = "DONE"
-                    boutsRemain[0][1][3] = "DONE"
-                break
-        if (isOver):
-            boutsRemain[0][1][2] = "WINNER OF THE 2017"
-            boutsRemain[0][1][3] = "NCAA FENCING CHAMPIONSHIP!!"
-        for i in range (1, schools):
-            # win difference
-            diff = int(boutsRemain[0][1][0]) - int(boutsRemain[i][1][0])
-            # 
-            iRemain = int(boutsRemain[i][1][1])
-            firstNeed = 1 + (int(boutsRemain[i][1][1]) + int(boutsRemain[i][1][0])) - int(boutsRemain[0][1][0])
-            firstRemain = int(boutsRemain[0][1][1])
-            # firstPercent = firstNeed / firstRemain * 100.0
-            if (diff > int(boutsRemain[i][1][1])):
-                boutsRemain[i][1][2] = diff
-                boutsRemain[i][1][3] = "OUT"
-            else:
-                if (iRemain > 0):
-                    iPercent = round(diff / iRemain * 100.0, 1)
-                    boutsRemain[i][1][2] = diff
-                    boutsRemain[i][1][3] = str(iPercent) + "%"
-                else:
-                    boutsRemain[i][1][2] = diff
-                    boutsRemain[i][1][3] = "OUT"
-        # for i in range(0, schools):
-            # if (int(boutsRemain[i][1][1]) == 0):
-                # boutsRemain[i][1][1] == "DONE"
-                # print ("Changed bouts remain for " + boutsRemain[i][0] + " to " + str(boutsRemain[i][1][1]))
-    else:
-        print ("derp...")
-    # name in quotes must be inside templates folder
-    return render_template("display.html", boutsRemain=boutsRemain)
 
 if __name__ == "__main__":
     app.run(debug = True)
