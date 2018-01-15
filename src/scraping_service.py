4ad97df713f4f897eae67c0f4dcf7cc0beff3072
tapegram <tapegram@gmail.com>
2018-01-15 16:40:31 -0500

    more refactoring

--- src/scraping_service.py
+++ src/scraping_service.py
@@ -16,60 +16,9 @@ def scrape_site_content_for_bout_data(content,
     remaining_bout_data = _update_current_school_rankings(remaining_bout_data)
     remaining_bout_data = _clean_references_to_image_files(remaining_bout_data,
                                                            school_map)
-
-    # find win differences in wins for teams and fill out array information 
-    isOver = True
-    for i in range (1, schools):
-        # if the difference is greater than all other schools' remaining bouts to fence then it is over
-        diff = int(remaining_bout_data[0][1][0]) - int(remaining_bout_data[i][1][0])
-        if (diff < int(remaining_bout_data[i][1][1])):
-            isOver = False
-            # bouts needed to clinch for top school becomes ith place's win total + ith place's remaining bouts - 1st place total bouts
-            need = 1 + int(remaining_bout_data[i][1][0]) + int(remaining_bout_data[i][1][1]) - int(remaining_bout_data[0][1][0])
-            remain = int(remaining_bout_data[0][1][1])
-            if (remain > 0):
-                remaining_bout_data[0][1][2] = need
-                clinchPercent = round(need / remain * 100.0, 1)
-                if (clinchPercent > 100):
-                    remaining_bout_data[0][1][3] = "Clinch impossible at this point"
-                else:
-                    remaining_bout_data[0][1][3] = str(round(need / remain * 100.0, 1)) + "%"
-            else:
-                remaining_bout_data[0][1][2] = "DONE"
-                remaining_bout_data[0][1][3] = "DONE"
-            break
-    if (isOver):
-        remaining_bout_data[0][1][2] = "WINNER OF THE 2017"
-        remaining_bout_data[0][1][3] = "NCAA FENCING CHAMPIONSHIP!!"
-    for i in range (1, schools):
-        # win difference
-        diff = int(remaining_bout_data[0][1][0]) - int(remaining_bout_data[i][1][0])
-        # 
-        iRemain = int(remaining_bout_data[i][1][1])
-        firstNeed = 1 + (int(remaining_bout_data[i][1][1]) + int(remaining_bout_data[i][1][0])) - int(remaining_bout_data[0][1][0])
-        firstRemain = int(remaining_bout_data[0][1][1])
-        # firstPercent = firstNeed / firstRemain * 100.0
-        if (diff > int(remaining_bout_data[i][1][1])):
-            remaining_bout_data[i][1][2] = diff
-            remaining_bout_data[i][1][3] = "OUT"
-        else:
-            if (iRemain > 0):
-                iPercent = round(diff / iRemain * 100.0, 1)
-                remaining_bout_data[i][1][2] = diff
-                remaining_bout_data[i][1][3] = str(iPercent) + "%"
-            else:
-                remaining_bout_data[i][1][2] = diff
-                remaining_bout_data[i][1][3] = "OUT"
-    # get percentage of wins so far
-    for i in range (0, schools):
-        # current wins / (total - remaining) * 100.0
-        totalBouts = (int(school_fencers_map[remaining_bout_data[i][0]]) * 23)
-        percent = round(remaining_bout_data[i][1][0] / (totalBouts - remaining_bout_data[i][1][1]) * 100.0, 1)
-        remaining_bout_data[i][1][0] = str(remaining_bout_data[i][1][0]) + " (" + str(percent) + "%)"
-    # for i in range(0, schools):
-        # if (int(remaining_bout_data[i][1][1]) == 0):
-            # remaining_bout_data[i][1][1] == "DONE"
-            # print ("Changed bouts remain for " + remaining_bout_data[i][0] + " to " + str(remaining_bout_data[i][1][1]))
+    remaining_bout_data = _calculate_win_differences(remaining_bout_data)
+    remaining_bout_data = _calculate_win_percentages(remaining_bout_data,
+                                                     school_fencers_map)
 
     return remaining_bout_data
 
@@ -106,5 +55,65 @@ def _clean_references_to_image_files(remaining_bout_data,
                                      school_map):
     for school_index in xrange(len(remaining_bout_data)):
         remaining_bout_data[school_index][1][4] = school_map[remaining_bout_data[school_index][0]] + ".png"
+
+    return remaining_bout_data
+
+
+def _calculate_win_differences(remaining_bout_data):
+    isOver = True
+    for school_index in xrange(1, len(remaining_bout_data)):
+        # if the difference is greater than all other schools' remaining bouts to fence then it is over
+        diff = int(remaining_bout_data[0][1][0]) - int(remaining_bout_data[school_index][1][0])
+        if (diff < int(remaining_bout_data[school_index][1][1])):
+            isOver = False
+            # bouts needed to clinch for top school becomes ith place's win total + ith place's remaining bouts - 1st place total bouts
+            need = 1 + int(remaining_bout_data[school_index][1][0]) + int(remaining_bout_data[school_index][1][1]) - int(remaining_bout_data[0][1][0])
+            remain = int(remaining_bout_data[0][1][1])
+            if (remain > 0):
+                remaining_bout_data[0][1][2] = need
+                clinchPercent = round(need / remain * 100.0, 1)
+                if (clinchPercent > 100):
+                    remaining_bout_data[0][1][3] = "Clinch impossible at this point"
+                else:
+                    remaining_bout_data[0][1][3] = str(round(need / remain * 100.0, 1)) + "%"
+            else:
+                remaining_bout_data[0][1][2] = "DONE"
+                remaining_bout_data[0][1][3] = "DONE"
+            break
+    if (isOver):
+        remaining_bout_data[0][1][2] = "WINNER OF THE 2017"
+        remaining_bout_data[0][1][3] = "NCAA FENCING CHAMPIONSHIP!!"
+    for school_index in xrange(1, len(remaining_bout_data)):
+        # win difference
+        diff = int(remaining_bout_data[0][1][0]) - int(remaining_bout_data[school_index][1][0])
+        # 
+        iRemain = int(remaining_bout_data[school_index][1][1])
+        firstNeed = 1 + (int(remaining_bout_data[school_index][1][1]) + int(remaining_bout_data[school_index][1][0])) - int(remaining_bout_data[0][1][0])
+        firstRemain = int(remaining_bout_data[0][1][1])
+        # firstPercent = firstNeed / firstRemain * 100.0
+        if (diff > int(remaining_bout_data[school_index][1][1])):
+            remaining_bout_data[school_index][1][2] = diff
+            remaining_bout_data[school_index][1][3] = "OUT"
+        else:
+            if (iRemain > 0):
+                iPercent = round(diff / iRemain * 100.0, 1)
+                remaining_bout_data[school_index][1][2] = diff
+                remaining_bout_data[school_index][1][3] = str(iPercent) + "%"
+            else:
+                remaining_bout_data[school_index][1][2] = diff
+                remaining_bout_data[school_index][1][3] = "OUT"
+
+
+    return remaining_bout_data
+
+
+def _calculate_win_percentages(remaining_bout_data,
+                               school_fencers_map):
+    for school_index in xrange(len(remaining_bout_data)):
+        totalBouts = (int(school_fencers_map[remaining_bout_data[school_index][0]]) * 23)
+        percent = round(remaining_bout_data[school_index][1][0] / (totalBouts -
+                                                                   remaining_bout_data[school_index][1][1]) * 100.0,
+                        1)
+        remaining_bout_data[school_index][1][0] = str(remaining_bout_data[school_index][1][0]) + " (" + str(percent) + "%)"
 
     return remaining_bout_data
